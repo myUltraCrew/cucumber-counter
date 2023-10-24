10893362843d0039a5b1e80d856a8fbbc13d8fa7
Sean Hagstrom <seanhagstrom1@gmail.com>
2023-10-24 10:37:53 -0400

    adds dynamic button click

--- tests/acceptance/features/simple_counter.feature
+++ tests/acceptance/features/simple_counter.feature
@@ -1,7 +1,21 @@
+# Basic example without using tables:
+# Feature: Simple counter
+#   User wants to use counter to increase or decrease the number on screen
+
+#   Scenario: Counter is increased
+#     Given User visits homepage
+#     When User clicks the + button
+#     Then User sees the counter get increased
+
 Feature: Simple counter
   User wants to use counter to increase or decrease the number on screen
 
-  Scenario: Counter is increased
+  Scenario: Counter is clicked
     Given User visits homepage
-    When User clicks the + button
-    Then User sees the counter get increased
+    When User clicks the "<name>" button
+    Then User sees the counter get "<name>"d
+
+  Examples:
+    | name     |
+    | increase |
+    | decrease |
--- tests/acceptance/step_definitions/simple_counter.steps.ts
+++ tests/acceptance/step_definitions/simple_counter.steps.ts
@@ -7,18 +7,42 @@ Given('User visits homepage', async function (this: ICustomWorld) {
   await page.goto('localhost:3000');
 });
 
-When('User clicks the + button', async function (this: ICustomWorld) {
-  const page = this.page!;
-  const plusButton = await page.locator('[data-testid="increase"]');
-  await expect(plusButton).toBeVisible();
-  await plusButton.click();
-});
+// When('User clicks the + button', async function (this: ICustomWorld) {
+//   const page = this.page!;
+//   const plusButton = await page.locator('[data-testid="increase"]');
+//   await expect(plusButton).toBeVisible();
+//   await plusButton.click();
+// });
+
+// Then(
+//   'User sees the counter get increased',
+//   async function (this: ICustomWorld) {
+//     const page = this.page!;
+//     const counterText = await page.locator('[data-testid="counter-text"]');
+//     await expect(counterText).toHaveText('Count: 1');
+//   }
+// );
+When(
+  'User clicks the {string} button',
+  async function (this: ICustomWorld, string: string) {
+    const page = this.page!;
+    const plusButton = await page.locator(`[data-testid="${string}"]`);
+    await expect(plusButton).toBeVisible();
+    await plusButton.click();
+  }
+);
 
 Then(
-  'User sees the counter get increased',
-  async function (this: ICustomWorld) {
+  'User sees the counter get {string}d',
+  async function (this: ICustomWorld, string: string) {
     const page = this.page!;
-    const counterText = await page.locator('[data-testid="counter-text"]');
-    await expect(counterText).toHaveText('Count: 1');
+    if (string === 'increase') {
+      const counterText = await page.locator(`[data-testid="counter-text"]`);
+      await expect(counterText).toHaveText('Count: 1');
+    }
+    if (string === 'decrease') {
+      const counterText = await page.locator(`[data-testid="counter-text"]`);
+      await expect(counterText).toHaveText('Count: 0');
+    }
   }
 );
